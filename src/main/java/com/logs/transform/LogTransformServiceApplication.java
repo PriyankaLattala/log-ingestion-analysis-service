99ef6927ec1909fe97074f86002361c7403d2777
PriyankaLattala <priyanka.lattala@outlook.com>
2025-09-30 21:11:11 +0200

    Initial commit java files

---
+++ src/main/java/com/logs/transform/LogTransformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.logs.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTransformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTransformServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logs/transform/LogTransformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logs.transform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTransformServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
